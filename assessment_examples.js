3b508c69a0cbdfd45a3c5190df1c0b02ccdb43b5
pn-engineering <admin@pn-engineering.com>
2017-08-24 14:30:52 -0700

    init commit

---
+++ assessment_examples.js
@@ -0,0 +1,64 @@
+//foo function
+function foo (arr, num) {
+  var findLength = arr.length;
+  if (findLength === num) {
+    console.log("True");
+  }
+  else {
+    console.log("False");
+  }
+}
+
+foo([0,1,4], 3);
+
+
+
+//iterate array function
+function iterate(arr) {
+
+var arr = [1,2,3,4];
+
+  for (var i=0; i < arr.length; i++) {
+    if (i < 1) {
+      console.log("first - ");
+    }
+    else if (i <= 2) {
+      console.log("not first or last - ");
+    }
+    else if (i = 3){
+      console.log("last");
+    }
+  }
+}
+
+iterate(arr);
+
+//for loop counter
+function count () {
+for (i=15; i >= 0; i--) {
+  console.log(i);
+}
+}
+count();
+
+// inner outer functions breakdown
+function outer(input) {
+  // 9 gets passed into this function
+  // var a = 9
+  var a = input;
+
+  function inner(multiplier) {
+    console.log(a * multiplier);
+  }
+  //return 9 * multiplier
+  return inner;
+}
+
+var firstResult = outer(9);
+firstResult(10);
+
+
+//object retrieval
+var someObject = {b : "some test",
+                  a : "this is A"};
+console.log(someObject.a);
---
+++ cats_loop.js
@@ -0,0 +1,18 @@
+var cats, noms;
+
+function treatDispenser(animals) {
+
+  var results = [];
+
+  for(var i=0; i < animals.length; i++){
+    return animals
+  }
+
+  return results;
+}
+
+cats = ['Jayne', 'Kaylee', 'Malcolm', 'Monty', 'River', 'Simon', 'Zoë'];
+
+noms = treatDispenser(cats);
+
+console.log(noms);
